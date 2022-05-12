c2b8af86cfd1634f415b5fa195f6517ed373467a
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-12 14:19:26 -0400

    Create GetGames and GetGamesById endpoint + Add Postman collection with tested endpoints and example responses

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,32 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetGames()
+        {
+            var videoGames = _context.VideoGames;
+            return Ok(videoGames);
+        }
+
+        [HttpGet("{gameId}")]
+        public IActionResult GetGamesById(int gameId)
+        {
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == gameId);
+            return Ok(videoGames);
+        }
+    }
+}
